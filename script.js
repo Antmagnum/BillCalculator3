c0e1624b40df8c23a9914a7e0df082d749006d41
Anthony <37840924+Antmagnum@users.noreply.github.com>
2022-09-14 19:01:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -44,15 +44,21 @@ btn2.addEventListener('click', function () {
 let btn2Custom = document.getElementById('btn2Custom');
 
 btn2Custom.addEventListener('click', function () {
-    let bill = document.getElementById('billAmount').value;
-    let peopleAmount = document.getElementById('peopleAmount').value;
-    let tipPercentage = document.getElementById('customTip').value;
-    let tipAmount = (bill * tipPercentage) / 100;
 
-    document.getElementById('customTip').value = tipPercentage;
-    document.getElementById('tipAmount').value = Math.ceil(tipAmount);
-    document.getElementById('totalBill').value = Math.ceil((parseFloat(bill) + parseFloat(tipAmount)) / parseFloat(peopleAmount));
-    document.getElementById('totalBillRoundCustom').value = Math.ceil(((parseFloat(bill) + parseFloat(tipAmount)) / parseFloat(peopleAmount)) / 10) * 10;
+    if (bill != 0) {
+        let bill = document.getElementById('billAmount').value;
+        let peopleAmount = document.getElementById('peopleAmount').value;
+        let tipPercentage = document.getElementById('customTip').value;
+        let tipAmount = (bill * tipPercentage) / 100;
+
+        document.getElementById('customTip').value = tipPercentage;
+        document.getElementById('tipAmount').value = Math.ceil(tipAmount);
+        document.getElementById('totalBill').value = Math.ceil((parseFloat(bill) + parseFloat(tipAmount)) / parseFloat(peopleAmount));
+        document.getElementById('totalBillRoundCustom').value = Math.ceil(((parseFloat(bill) + parseFloat(tipAmount)) / parseFloat(peopleAmount)) / 10) * 10;
+    } else {
+
+
+    }
 })
 
 
